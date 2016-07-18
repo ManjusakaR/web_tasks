580fac4e254b5b783ecb6ce982e6605dc4deaf1d
WangRui <857851982@qq.com>
2016-07-18 18:48:51 +0800

    student data cache hash

--- B0/student.rb
+++ B0/student.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 class Student
   @@n=0
   @id=0
@@ -35,13 +36,49 @@ class Student
   end
 
 end
+
+@d=Dir.pwd
+puts @d
+@filename=@d+'/student.yml'
+puts @filename
+if File.exists?(@filename)==false then
+  @file=File.new(@filename,"w")
 @i=1
+@configuration=Hash.new
 while @i<=100 do
 s=Student.new()
-s.i()
-s.n()
-s.g()
-s.a()
-s.d()
+ss='st'+@i.to_s
+@con={ ss => {
+  'id' => s.i(),
+  'name' => s.n().to_s,
+  'gender'  => s.g().to_s,
+  'age'  =>  s.a()
+}
+}
+ @configuration.update(@con)
+ #puts @configuration
+#str=+' '++' '++' '+
+#@file.puts "#{str}"
+#aFile.syswrite("ABCDEF")
+#s.d()
 @i+=1
 end
+puts "#@configuration"
+open(@filename, 'a+') { |f| YAML.dump(@configuration, f) }
+#open(@filename) { |f| puts f.read }
+open(@filename) { |f| YAML.load(f) }
+else
+   #open(@filename) { |f| puts f.read }
+   std=YAML.load(File.open(@filename))
+   puts "#{std}"
+
+  #puts "problem id is s#{problem["program"]["id"]}"
+end
+
+
+
+#puts "sort by id?(y/n)"
+#if gets==y||gets==Y
+#
+#puts "sort by age?(y/n)"
+#puts "sort by name?(y/n)"
