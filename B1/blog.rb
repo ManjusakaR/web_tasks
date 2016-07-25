387e33733d16d75e0712a2db1d0683a9d538a352
WangRui <857851982@qq.com>
2016-07-25 18:12:25 +0800

    most success except checkbox

--- B1/blog.rb
+++ B1/blog.rb
@@ -4,7 +4,7 @@ require 'date'
 
 $blog_a=Array.new
 $b_search=Array.new
-
+$msg=''
 class Blog
   @@b_id=0
   @id=0
@@ -12,7 +12,7 @@ class Blog
   @author=''
   @created_at=Time.new
 
-  def initialize( msg, aut)
+  def initialize(msg, aut)
     @id=@@b_id
     @@b_id+=1
     @message=msg
@@ -37,39 +37,73 @@ class Blog
   end
 
   def Blog.add_blog(b_new)
-    blog_new=[[b_new.get_id,b_new.get_message,b_new.get_author,b_new.get_time]]
+    $msg=''
+    if b_new.get_author==""&&b_new.get_message.size<10
+      $msg='author_name is nil and the length of message is less than 10'
+    elsif b_new.get_author==""
+      $msg='author_name is nil'
+    elsif b_new.get_message.size<10
+      $msg='the length of message is less than 10'
+    else
+      $blog_a+=blog_new=[[b_new.get_id,b_new.get_message,b_new.get_author,b_new.get_time]]
+      $b_search=$blog_a
+      $msg='add message sucessfilly'
+    end
+    b_new.get_id
   end
 
   def Blog.search_by_id(id,blog_a)
     b_id=Array.new
+    i=0
     blog_a.each{ |ba|
       if id==ba[0]
-        b_id=ba
+        i=1
+        b_id+=[ba]
         break
       end
       }
+      if i==0
+        $msg='no such id can search'
+      else
+        $msg='success searched'
+      end
       b_id
   end
 
   def Blog.search_by_author(aut,blog_a)
     b_aut=Array.new
+    i=0
     blog_a.each{ |ba|
       if aut==ba[2]
-        b_aut=ba
-        break
+        i=1
+        b_aut+=[ba]
       end
     }
+    if i==0
+      $msg='no such author can search'
+    else
+      $msg='success searched'
+    end
     b_aut
   end
 
   def Blog.delete_blog(id,blog_a)
+    num=0
+    msg=''
+    id.each{ |b|
         blog_a.each{ |ba|
-          if id==ba[0]
+          if b[1].to_i==ba[0]
+            num+=1
             blog_a.delete(ba)
             break
           end
         }
-      blog_a
+      }
+      if num==0
+        $msg='Warning, No such message here you can delete!'
+      else
+        $msg="You success to delete #{num} message/s."
+      end
   end
 
 end
@@ -79,7 +113,9 @@ get '/' do
 end
 
 get '/show' do
-  $blog_a
+  $msg=''
+  $b_search=$blog_a
+  $msg='All message'
   erb :show
 end
 
@@ -91,8 +127,10 @@ post '/show' do
   elsif author!=""
     $b_search=Blog.search_by_author(author,$blog_a)
   else
+    $msg='All message'
     $b_search=$blog_a
   end
+  $b_search
   erb :show
 end
 
@@ -102,7 +140,9 @@ end
 
 post '/add' do
   b_new=Blog.new(params[:message],params[:author])
-  $blog_a+=Blog.add_blog(b_new)
+  $b_id=Blog.add_blog(b_new)
+  $blog_a
+  $b_search
   erb :show
 end
 
@@ -111,7 +151,7 @@ get '/delete' do
 end
 
 post '/delete' do
-  id=params[:id]
-  Blog.delete_blog(id.to_i,$blog_a)
+  id=params
+  $msg=Blog.delete_blog(id.to_a,$blog_a)
   erb :show
 end
